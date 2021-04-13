349e00090ae0ea4528f725157f7b8010f7eafa6b
niemet0502 <vincentmarius8@gmail.com>
2021-04-13 22:08:33 +0200

    main :: added link on navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
 import logo from "./img/logo.png"
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
 
 function Nav() {
   const [show, handleShow] = useState(false);
@@ -17,16 +18,26 @@ function Nav() {
   }, []);
   return (
     <div className={`nav ${show && "nav__black"}`}>
-      <img
-        className="nav__logo"
-        src={logo}
-        alt="netflix_logo"
-      />
+      <div className="nav_link_container">
+        <img
+          className="nav__logo"
+          src={logo}
+          alt="netflix_logo"
+        />
+        <a href="#!" className="active">Home</a>
+        <a href="">TV Shows</a>
+        <a href="">Movie</a>
+        <a href="">Latest</a>
+        <a href="">My List</a>
+      </div>
+      <div>
+      <CardGiftcardIcon />
       <img
         className="nav__avatar"
         src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
         alt="netflix avatar"
       />
+      </div>
     </div>
   );
 }
