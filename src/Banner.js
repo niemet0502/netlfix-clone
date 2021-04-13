876e692335e3eb2cee2321b53663704b3dd31927
niemet0502 <vincentmarius8@gmail.com>
2021-04-13 23:08:39 +0200

    main :: displayed subtitle on Banner

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./Request";
 import "./Banner.css";
+import logo from "./img/logo2.png"
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -32,6 +33,10 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
+        <h1 style={{display: 'flex', alignItems: 'center'}}> 
+          <img src={logo} style={{width: '25px'}} /> 
+          <span style={{color: '#e5e5e5', fontSize: '14px'}}>SERIES</span>
+        </h1>
         <h1 className="banner__title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
